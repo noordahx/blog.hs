d67e0ca7191f5f4070ddab010b4bfbff58726edf
Nurdaulet <taumergenov@outlook.com>
2023-12-18 00:09:13 +0800

    add paragraph only markup parser

---
+++ Markup.hs
@@ -0,0 +1,38 @@
+module Markup
+    ( Document
+    , Structure(..)
+    )
+where
+    
+import Numeric.Natural
+
+type Document
+    = [Structure]
+
+data Structure
+    = Heading Natural String
+    | Paragraph String
+    | UnorderedList [String]
+    | OrderedList [String]
+    | CodeBlock [String]
+    deriving Show
+
+parse :: String -> Document
+parse = parseLines [] . lines
+
+parseLines :: [String] -> [String] -> Document
+parseLines currentParagraph txts =
+    let
+        paragraph = Paragraph (unlines (reverse currentParagraph))
+    in
+        case txts of
+            [] -> [paragraph]
+            currentLine : rest ->
+                if trim currentLine == ""
+                    then
+                        paragraph : parseLines [] rest
+                    else
+                        parseLines (currentLine : currentParagraph) rest
+
+trim :: String -> String
+trim = unwords . words
