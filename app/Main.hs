fa3e5465b81ea85d8b48856199ee32e10d3f0736
Nurdaulet <taumergenov@outlook.com>
2024-01-22 23:41:34 +0800

    add buildIndex fm

--- app/Main.hs
+++ app/Main.hs
@@ -52,3 +52,26 @@ confirm =
                 "n" -> pure False
                 _ -> putStrLn "Invalid response. use y or n" *>
                     confirm
+
+buildIndex :: [(FilePath, Markup.Document)] -> Html.Html
+buildIndex files =
+    let 
+        previews =
+            map
+            ( \(file, doc) ->
+                case doc of
+                    Markup.Heading 1 heading : article ->
+                        Html.h_ 3 (Html.link_ file (Html.txt_ heading))
+                        <> foldMap convertStructure (take 3 article)
+                        <> Html.p_ (Html.link_ file (Html.txt_ "..."))
+                    _->
+                        Html.h_ 3 (Html.link_ file (Html.txt_ file))
+            )
+            files
+        in
+            Html.html_
+                "Blog"
+                ( Html.h_ 1 (Html.link_ "index.html" (Html.txt_ "Blog"))
+                <> Html.h_ 2 (Html.txt_ "Posts")
+                <> mconcat previews
+            )
